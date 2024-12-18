56917f3cb41c7be1fa5007e510cf2bf8ce78ad0a
Flourish38 <35986588+Flourish38@users.noreply.github.com>
2024-12-18 12:54:39 -0500

    day 10

---
+++ julia/src/10-2.jl
@@ -0,0 +1,31 @@
+input = readlines("input/10.txt")
+data = parse.(Int, reduce(hcat, collect.(input)))
+
+trailheads = findall(==(0), data)
+
+function neighbors(position)
+    pos = [Tuple(position)...]
+    return map(x -> CartesianIndex(x...), [pos .+ [0, 1], pos .+ [1, 0], pos .- [0, 1], pos .- [1, 0]])
+end
+
+function recursive_search(position)
+    height = data[position]
+    if height == 9
+        return [position]
+    end
+    accum = []
+    for neighbor in neighbors(position)
+        if checkbounds(Bool, data, neighbor) && data[neighbor] == height + 1
+            append!(accum, recursive_search(neighbor))
+        end
+    end
+    return accum
+end
+
+begin
+    result = 0
+    for trailhead in trailheads
+        result += length(recursive_search(trailhead))
+    end
+    println(result)
+end
