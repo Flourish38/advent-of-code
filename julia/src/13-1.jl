0cfa4cb63e7be2853f299e86bc2ed2cab91b4d78
Flourish38 <35986588+Flourish38@users.noreply.github.com>
2024-12-18 19:41:12 -0500

    refactor because julia was made for this I love this language so much istg no other language does this

--- julia/src/13-1.jl
+++ julia/src/13-1.jl
@@ -1,19 +1,10 @@
 input = split.(strip.(split(read("input/13.txt", String), "\n\n")), '\n')
-
 regex = r"[^0-9]+([0-9]+)[^0-9]+([0-9]+)"
 
-begin
-    result = 0
-    for (A, B, P) in input
-        a = parse.(Int, match(regex, A))
-        b = parse.(Int, match(regex, B))
-        AB = hcat(a, b)
-        p = parse.(Int, match(regex, P))
-        n = AB \ p # linear algebra, baybee! AB * n = p
-        if all(isapprox.(n, round.(Int, n)))
-            n = round.(Int, n)
-            result += sum(n .* [3, 1])
-        end
-    end
-    println(result)
-end
+# LOOK AT HOW PRETTY THIS IS
+sum(input) do P
+    a, b, p = (x -> parse.(Int, match(regex, x))).(P)
+    AB = Rational.(hcat(a, b))
+    n = AB \ p
+    return all(isinteger.(n)) ? sum(Int.(n) .* [3, 1]) : 0
+end |> println
--- julia/src/13-2.jl
+++ julia/src/13-2.jl
@@ -2,17 +2,12 @@ input = split.(strip.(split(read("input/13.txt", String), "\n\n")), '\n')
 
 regex = r"[^0-9]+([0-9]+)[^0-9]+([0-9]+)"
 
-begin
-    result = 0
-    for (A, B, P) in input
-        a = parse.(Int, match(regex, A))
-        b = parse.(Int, match(regex, B))
-        AB = hcat(a, b)
-        p = parse.(Int, match(regex, P)) .+ 10000000000000 # 1e13
-        n = round.(Int, AB \ p) # linear algebra, baybee! AB * n = p
-        if AB * n == p  # Have to double check with integers because floating point error is very significant here
-            result += sum(n .* [3, 1])
-        end
-    end
-    println(result)
-end
+sum(input) do P
+    a, b, p = (x -> parse.(Int, match(regex, x))).(P) # 3 lines of input per problem
+    # God I love this language. Getting floating point error? Just use different numbers! ^_^
+    AB = Rational.(hcat(a, b))
+    p .+= 10000000000000 # 1e13
+    n = AB \ p # Linear algebra, baybee! AB * n = p
+    # Beautiful.
+    return all(isinteger.(n)) ? sum(Int.(n) .* [3, 1]) : 0
+end |> println
