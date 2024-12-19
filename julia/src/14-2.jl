8c36628d3723a9de87230b558ffebd2aa269bf26
Flourish38 <35986588+Flourish38@users.noreply.github.com>
2024-12-19 15:50:23 -0500

    do an actual statistical test instead of looking at the pictures manually

--- julia/src/14-2.jl
+++ julia/src/14-2.jl
@@ -1,49 +1,10 @@
-using UnicodePlots
+using HypothesisTests, UnicodePlots
 
 input = readlines("input/14.txt")
 
 regex = r"p=([\-0-9]+),([\-0-9]+) v=([\-0-9]+),([\-0-9]+)"
 
-dims = [101, 103]
-
-begin
-    robots = map(input) do line
-        px, py, vx, vy = parse.(Int, match(regex, line))
-        p, v = [px, py], [vx, vy]
-    end
-
-    it = 0
-    while true
-        it += 1
-        for (p, v) in robots
-            p .= mod.((p .+ v), dims)
-        end
-        xs, ys = reduce(first.(robots), init=[[], []]) do (xs, ys), (x, y)
-            push!(xs, x)
-            push!(ys, y)
-            xs, ys
-        end
-        println(scatterplot(xs, ys, xlim=(0, dims[1]), ylim=(0, dims[2])))
-        println(it)
-        sleep(0.25)
-    end
-end
-
-# OH
-# x dist is starting at 4 and every 101 after
-# y dist is starting at 76 and every 103 after
-
-begin
-    start = [4, 76]
-    n = 0
-    while (dims[2] * n + start[2] - start[1]) % dims[1] != 0
-        n += 1
-    end
-    timesteps = dims[2] * n + start[2]
-    println(timesteps)
-end
-
-begin
+function print_timestep(timesteps)
     robots = map(input) do line
         px, py, vx, vy = parse.(Int, match(regex, line))
         p, v = [px, py], [vx, vy]
@@ -60,7 +21,38 @@ begin
     println(scatterplot(xs, ys, xlim=(0, dims[1]), ylim=(0, dims[2])))
 end
 
-# I didn't like this one very much.
-# I probably could have tried to do it statistically but I'm unfamiliar with exactly how to do that.
-# ...I should probably learn how.
-# Fortunately, it was guaranteed that I would see an anomaly within maximum(dims) timesteps, which isn't that long.
+dims = [101, 103]
+begin
+    counts = zeros.(Int, dims)
+    robots = map(input) do line
+        px, py, vx, vy = parse.(Int, match(regex, line))
+        p, v = [px, py], [vx, vy]
+        counts[1][p[1]+1] += 1
+        counts[2][p[2]+1] += 1
+        (p, v)
+    end
+
+    it = 0
+    min_p = 1.0
+    while true
+        it += 1
+        for (p, v) in robots
+            counts[1][p[1]+1] -= 1
+            counts[2][p[2]+1] -= 1
+            p .= mod.((p .+ v), dims)
+            counts[1][p[1]+1] += 1
+            counts[2][p[2]+1] += 1
+        end
+        # Check p-value of each dimension independently being uniform
+        p_data = prod(pvalue.(ChisqTest.(counts)))
+        if p_data < min_p
+            min_p = p_data
+            print_timestep(it)
+            println(it, "\t", min_p)
+        end
+        # just so you can interrupt it.
+        sleep(0.000001)
+    end
+end
+
+# Chi squared test is not the perfect test for this, since it does not encode the spatial nature of the problem, but it is more than good enough.
