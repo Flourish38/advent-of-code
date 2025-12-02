009bf05366745efbc4bdfffc6cd48275683baadf
Flourish38 <emisandvik@gmail.com>
2025-12-02 14:02:57 -0500

    Day 1

---
+++ 2025/1-1.zig
@@ -0,0 +1,28 @@
+const std = @import("std");
+
+pub fn main() !void {
+    var path_buf: [256]u8 = undefined;
+    const cwd = std.fs.cwd();
+    const path = try cwd.realpath("input/1.txt", &path_buf);
+    const file = try std.fs.openFileAbsolute(path, .{});
+    var reader_buf: [4096]u8 = undefined;
+    var reader = file.reader(&reader_buf);
+    var pos_acc: i16 = 50;
+    var zero_acc: u64 = 0;
+    while (reader.interface.takeDelimiterExclusive('\r')) |line| {
+        const relevant_digits: []const u8 = if (line.len == 2) line[1..2] else line[line.len - 2 .. line.len];
+        const parsed = try std.fmt.parseInt(i16, relevant_digits, 0);
+        switch (line[0]) {
+            'R' => pos_acc += parsed,
+            'L' => pos_acc -= parsed,
+            else => unreachable,
+        }
+        pos_acc = @mod(pos_acc, 100);
+        if (pos_acc == 0) zero_acc += 1;
+        _ = try reader.interface.discard(.limited(2));
+    } else |err| switch (err) {
+        error.EndOfStream => {},
+        else => return err,
+    }
+    std.debug.print("{d}", .{zero_acc});
+}
---
+++ 2025/1-2.zig
@@ -0,0 +1,39 @@
+const std = @import("std");
+
+pub fn main() !void {
+    var path_buf: [256]u8 = undefined;
+    const cwd = std.fs.cwd();
+    const path = try cwd.realpath("input/1.txt", &path_buf);
+    const file = try std.fs.openFileAbsolute(path, .{});
+    var reader_buf: [4096]u8 = undefined;
+    var reader = file.reader(&reader_buf);
+    var pos_acc: i16 = 50;
+    var zero_acc: u64 = 0;
+    var is_zero = false;
+    while (reader.interface.takeDelimiterExclusive('\r')) |line| {
+        const relevant_offset = @max(1, line.len - 2);
+        const relevant_digits = line[relevant_offset..];
+
+        const parsed = try std.fmt.parseInt(i16, relevant_digits, 10);
+        switch (line[0]) {
+            'R' => pos_acc += parsed,
+            'L' => pos_acc -= parsed,
+            else => unreachable,
+        }
+        const fixed_acc = @mod(pos_acc, 100);
+        if (fixed_acc == 0 or (!is_zero and fixed_acc != pos_acc)) zero_acc += 1;
+        // std.debug.print("{s}\t{d}\t{d}\t{any}\t{d}\n", .{ line, pos_acc, fixed_acc, is_zero, zero_acc });
+        pos_acc = fixed_acc;
+        is_zero = pos_acc == 0;
+
+        const other_digits = line[1..relevant_offset];
+        if (other_digits.len > 0)
+            zero_acc += try std.fmt.parseInt(u8, other_digits, 10);
+
+        _ = try reader.interface.discard(.limited(2));
+    } else |err| switch (err) {
+        error.EndOfStream => {},
+        else => return err,
+    }
+    std.debug.print("{d}", .{zero_acc});
+}
