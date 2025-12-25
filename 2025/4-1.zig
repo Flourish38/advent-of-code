0900bb278de4398bebef6acab3fe78b504da6faa
Flourish38 <emisandvik@gmail.com>
2025-12-25 13:15:09 -0500

    4x speed, much better!

--- 2025/4-1.zig
+++ 2025/4-1.zig
@@ -3,36 +3,83 @@ const assert = std.debug.assert;
 
 const input = @embedFile("input/4.txt");
 
+const Vec = @Vector(32, u8);
+
+const Adder = struct {
+    result: u192,
+    carry: u192,
+    parity: u192,
+
+    const zero: Adder = .{ .result = 0, .carry = 0, .parity = 0 };
+
+    pub fn intake(self: *Adder, arr: []const u192) void {
+        for (arr) |n| {
+            const carry = self.parity & n;
+            self.result |= carry & self.carry;
+            self.carry ^= carry;
+            self.parity ^= n;
+            // if (arr.len == 2)
+            //     std.debug.print("{b:0>11}             ", .{n})
+            // else
+            //     std.debug.print("{b:0>11} ", .{n});
+        }
+        // if (arr.len == 2)
+        //     std.debug.print("\x08\x08\x08\x08\x08\x08\x08\x08\x08\x08\x08\x08{d}, {d}, {d}\n", .{ @popCount(self.result), @popCount(self.carry), @popCount(self.parity) })
+        // else
+        //     std.debug.print("{d}, {d}, {d}\n", .{ @popCount(self.result), @popCount(self.carry), @popCount(self.parity) });
+    }
+};
+
 pub fn main() !void {
     var timer = try std.time.Timer.start();
     var acc: usize = 0;
-    var lines: [3][]const u8 = @splat(&.{});
-    var it = std.mem.tokenizeAny(u8, input, "\r\n");
-    // var test_line: [128]u8 = @splat(' ');
-    for (lines[1..]) |*line| {
-        line.* = it.next().?;
-    }
-    while (it.next()) |new_line| {
-        lines[0] = lines[1];
-        lines[1] = lines[2];
-        lines[2] = new_line;
-
-        for (lines[1][1 .. lines[1].len - 1], 0..) |c, i| {
-            if (c != '@') {
+    const n = std.mem.indexOfScalar(u8, input, '\n').? + 1;
+    const line_len = input.len / n;
+    var prev_lines: [3]u192 = @splat(0);
+    var prev_above: [3]u192 = @splat(0);
+    for (0..line_len) |line_idx| {
+        const offset = n * line_idx;
+        var line: u192 = 0;
+        for (input[offset..][0 .. n - 2]) |c| {
+            if (c == '@')
+                line |= 1
+            else
                 assert(c == '.');
-                continue;
-            }
-            var num_rolls: usize = 0;
-            for (lines) |line| {
-                num_rolls += std.mem.count(u8, line[i .. i + 3], "@");
-            }
-            if (num_rolls < 4 + 1) {
-                acc += 1;
-                // test_line[i + 1] = 'x';
-            }
+            line <<= 1;
         }
-        // std.debug.print("{s}\n{s}\n{s}\n{s}\n\n", .{ lines[0], lines[1], lines[2], test_line });
-        // test_line = @splat(' ');
+        const lines: [3]u192 = .{
+            line << 1,
+            line,
+            line >> 1,
+        };
+        defer prev_lines = lines;
+
+        var adder: Adder = .zero;
+        adder.intake(&prev_above);
+        adder.intake(&.{
+            prev_lines[0] & prev_lines[1],
+            prev_lines[2] & prev_lines[1],
+        });
+        adder.intake(&.{
+            prev_lines[1] & lines[0],
+            prev_lines[1] & lines[1],
+            prev_lines[1] & lines[2],
+        });
+        prev_above = .{
+            lines[1] & prev_lines[0],
+            lines[1] & prev_lines[1],
+            lines[1] & prev_lines[2],
+        };
+        acc += @popCount(prev_lines[1] & ~adder.result);
+        // std.debug.print("{b:0>11}, {b:0>11}\t{b:0>11}\t{d}\n", .{ prev_lines[1], adder.result, prev_lines[1] & ~adder.result, acc });
     }
+    var adder: Adder = .zero;
+    adder.intake(&prev_above);
+    adder.intake(&.{
+        prev_lines[0] & prev_lines[1],
+        prev_lines[2] & prev_lines[1],
+    });
+    acc += @popCount(prev_lines[1] & ~adder.result);
+    // std.debug.print("{b:0>11}, {b:0>11}\t{b:0>11}\t{d}\n", .{ prev_lines[1], adder.result, prev_lines[1] & ~adder.result, acc });
     std.debug.print("{D}\n{d}", .{ timer.lap(), acc });
 }
